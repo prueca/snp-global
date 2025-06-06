a53adfbb72a47bc9696c365d1d0ec2fc63a03b03
Peter John Rueca <peter.rueca@safetychampion.online>
2025-06-06 15:24:33 +0800

    use skiprows

--- conso.py
+++ conso.py
@@ -2,8 +2,8 @@ import os
 import glob
 import pandas as pd
 
-loc = input("Locate excel files: ")
-# loc = r"~/Documents/excel"
+# loc = input("Locate excel files: ")
+loc = r"~/Documents/excel"
 loc = os.path.expanduser(loc)
 
 all_files = glob.glob(os.path.join(f"{loc}", "*.xls*"))
@@ -13,20 +13,17 @@ output = r"./out.xlsx"
 # output = os.path.join(f"{loc}", "output.xlsx")
 
 try:
-    with pd.ExcelWriter(output) as writer:
+    with pd.ExcelWriter(output, engine="xlsxwriter") as writer:
         is_first = True
 
         for file in all_files:
-            xl = pd.read_excel(file, header=None)
+            xl = pd.read_excel(file, header=None, skiprows=3)
 
-            if is_first == False:
-                # drop row 1-3
-                xl.drop(index=0, axis=0, inplace=True)
-                xl.drop(index=1, axis=0, inplace=True)
-                xl.drop(index=2, axis=0, inplace=True)
+            if is_first:
+                xl = pd.read_excel(file, header=None)
+                is_first = False
 
             all.append(xl)
-            is_first = False
 
         combined = pd.concat(all, ignore_index=True)
         combined.to_excel(writer, sheet_name="Sheet1", index=False, header=None)
